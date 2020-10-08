230d696f06259c39cb59cf65b52a60b405b4f92b
erickmx <erickblanc96@gmail.com>
2020-10-08 17:41:06 -0500

    implemented functionality to save and sum values in a slice

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n uint16
+	var sum int32
+	var i uint16
+
+	fmt.Scan(&n)
+
+	s := make([]int32, n)
+
+	for i = 0; i < n; i++ {
+		fmt.Scan(&s[i])
+	}
+
+	for i = 0; i < n; i++ {
+		sum = sum + s[i]
+	}
+
+	fmt.Print(sum)
 }
